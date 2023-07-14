35b5e75ffb6a7aa47d1762faceef7c6b61f4606b
kridt <69456983+kridt@users.noreply.github.com>
2023-07-15 00:01:05 +0200

    .

--- src/pages/Stempling.js
+++ src/pages/Stempling.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { database } from "../firebase";
 
 export default function Stempling() {
   /* const [currentDate, setCurrentDate] = useState(""); */
+  const todaysCode = new Date().toLocaleDateString().replaceAll(".", "");
+  const [todaysStempel, setTodaysStempel] = React.useState();
   const currentYear = new Date().getFullYear();
   const currentMonth = new Date().getMonth();
   const [currentStempel, setCurrentStempel] = React.useState({
@@ -12,16 +14,25 @@ export default function Stempling() {
     location: { latitude: "", longitude: "" },
     funktion: "",
   });
-  const getAllDaysInMonth = (month, year) =>
-    Array.from(
-      { length: new Date(year, month, 0).getDate() },
-      (_, i) => new Date(year, month - 1, i + 1)
-    );
-  const daysInMonth = getAllDaysInMonth(currentMonth, currentYear);
 
-  console.log(daysInMonth);
+  useEffect(() => {
+    database
+      .collection("users")
+      .doc("kridt")
+      .collection("stempel")
+      .doc(todaysCode)
+      .get()
+      .then((doc) => {
+        setCurrentStempel(doc.data());
+      });
+  }, []);
 
+  console.log(currentStempel);
   function handleSteplIn() {
+    const sixDigitDateCode = new Date()
+      .toLocaleDateString()
+      .replaceAll(".", "");
+    console.log(sixDigitDateCode);
     navigator.geolocation.getCurrentPosition((position) => {
       const time = new Date().toLocaleTimeString("da-DK");
       const dato = new Date().toLocaleDateString("da-DK");
@@ -36,11 +47,23 @@ export default function Stempling() {
         time,
         location,
       };
+
+      database
+        .collection("users")
+        .doc("kridt")
+        .collection("stempel")
+        .doc(sixDigitDateCode)
+        .set({
+          "stempling ind": stempel,
+        });
       setCurrentStempel(stempel);
     });
   }
 
   function handleSteplOut() {
+    const sixDigitDateCode = new Date()
+      .toLocaleDateString()
+      .replaceAll(".", "");
     navigator.geolocation.getCurrentPosition((position) => {
       const time = new Date().toLocaleTimeString("da-DK");
       const dato = new Date().toLocaleDateString("da-DK");
@@ -55,6 +78,15 @@ export default function Stempling() {
         time,
         location,
       };
+      database
+        .collection("users")
+        .doc("kridt")
+        .collection("stempel")
+        .doc(sixDigitDateCode)
+        .set({
+          "stempling ind": currentStempel,
+          "stempling ud": stempel,
+        });
       setCurrentStempel(stempel);
     });
   }
@@ -65,11 +97,11 @@ export default function Stempling() {
       <button onClick={() => handleSteplIn()}>Stempl ind</button>
       <button onClick={() => handleSteplOut()}>Stepl Ud</button>
       <h2>Dit seneste stempel</h2>
-      <p>Funktion: {currentStempel.funktion}</p>
-      <p>Dato: {currentStempel.dato}</p>
-      <p>Tid: {currentStempel.time}</p>
-      <p>Latitude: {currentStempel.location.latitude}</p>
-      <p>Longitude: {currentStempel.location.longitude}</p>
+      <p>Funktion: {currentStempel?.funktion}</p>
+      <p>Dato: {currentStempel?.dato}</p>
+      <p>Tid: {currentStempel?.time}</p>
+      <p>Latitude: {currentStempel?.location?.latitude}</p>
+      <p>Longitude: {currentStempel?.location?.longitude}</p>
     </div>
   );
 }
