18963556cbc606b76366f00385d910b7b7cc9a6c
Christian Nielsen <chrnielsen2003@gmail.com>
2023-07-23 14:31:28 +0200

    .

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { database } from "../firebase";
 import AdminNav from "../components/AdminNav";
+import { Link } from "react-router-dom";
 
 export default function Admin() {
   const [allUsers, setAllUsers] = useState([]);
@@ -24,6 +25,41 @@ export default function Admin() {
     <div>
       <AdminNav />
       <h1>Admin</h1>
+      <h2>Liste med alle medarbejdere, tryk på dem for at ændre</h2>
+      <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <p>Navn</p>
+          <p>Medarbejder nummer</p>
+          <p>Time løn</p>
+        </div>
+        {allUsers.map((user) => {
+          return (
+            <Link
+              to={`/editCorworker/${user.medarbejderNummer}`}
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              <div
+                style={{
+                  backgroundColor: "#555",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  marginBottom: "2em",
+                }}
+                key={user.medarbejderNummer}
+              >
+                <p>{user.name}</p>
+                <p>{user.medarbejderNummer}</p>
+                <p>{user.timeLøn} kr</p>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
