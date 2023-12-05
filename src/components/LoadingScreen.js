981eb10239e05fd49268d95083bb76ab4e2c471e
Christian Nielsen <chrnielsen2003@gmail.com>
2023-12-05 01:02:41 +0100

    asd

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Audio } from "react-loader-spinner";
 
 export default function LoadingScreen() {
   const style = {
@@ -7,9 +8,21 @@ export default function LoadingScreen() {
     left: "0",
     width: "100%",
     height: "100%",
-    backgroundColor: "white",
+    backgroundColor: "grey",
     opacity: "0.5",
   };
 
-  return <div style={style}>LoadingScreen</div>;
+  return (
+    <div style={style}>
+      <div style={{ position: "fixed", top: "50%", left: "50%" }}>
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="black"
+          ariaLabel="loading"
+        />
+      </div>
+    </div>
+  );
 }
